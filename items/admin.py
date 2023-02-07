a98a764b10b46d16f2ebc4e053ace9d77f0449fe
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-07 13:20:00 +0100

    Commit 2 Items

--- items/admin.py
+++ items/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Category
+from .models import Category, Item
 
 admin.site.register(Category)
+admin.site.register(Item)
 
