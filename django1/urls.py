fca19dedee8d6f276d6bdc9f174c8a5267a6619c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-07 10:26:46 +0100

    Commit 2 Items

--- django1/urls.py
+++ django1/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 from django1 import settings
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('items.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
