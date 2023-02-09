e220ae37d080d9ec344a2454c143f576f255aad4
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-09 09:40:41 +0100

    Commit 6 delete

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from items.models import Item
 
@@ -8,6 +8,9 @@ from items.models import Item
 
 @login_required
 def index(request):
-    items = Item.objects.filter(username=request.user)
+    items = Item.objects.filter(created_by=request.user)
 
     return render(request,'dashboard/index.html',{'items':items})
+
+
+
