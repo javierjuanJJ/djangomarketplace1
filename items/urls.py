0c54489906d5cb7ddf32b7db80bbf9f448ddbaf3
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-09 19:35:51 +0100

    Commit 4 Log in

--- items/urls.py
+++ items/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('new/',views.new, name='new'),
     path('<int:pk>/',views.detail, name='detail'),
     path('<int:pk>/delete/',views.delete, name='delete'),
-    path('<int:pk>/edit/',views.delete, name='edit'),
+    path('items/',views.items, name='items'),
 ]
