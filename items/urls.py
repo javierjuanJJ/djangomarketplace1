a6dfb8173e7687aff05bb00772cc42c2e42e7d11
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-09 10:04:10 +0100

    Commit 7 items

--- items/urls.py
+++ items/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 app_name = 'item'
 urlpatterns = [
+    path('',views.items, name='index'),
     path('new/',views.new, name='new'),
     path('<int:pk>/',views.detail, name='detail'),
     path('<int:pk>/delete/',views.delete, name='delete'),
