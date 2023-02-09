614322938c9c8c0a736ac6a38b529b6fe29880af
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-09 09:46:16 +0100

    Commit 7 edit

--- items/urls.py
+++ items/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('new/',views.new, name='new'),
     path('<int:pk>/',views.detail, name='detail'),
     path('<int:pk>/delete/',views.delete, name='delete'),
-
+    path('<int:pk>/edit/',views.delete, name='edit'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from items.forms import NewItemForm
+from items.forms import NewItemForm, EditItemForm
 from items.models import Item
 
 
@@ -33,6 +33,27 @@ def new(request):
         'form': form
     })
 
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+
+    return render(request, 'item/form.html',{
+        'form': form,
+        'title': 'Edit item'
+    })
 
 @login_required
 def delete(request, pk):
