ace92005e360b9cf319f1e236b64980cdf6e7c65
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-09 19:48:39 +0100

    Commit 4 Log in

--- items/views.py
+++ items/views.py
@@ -18,7 +18,7 @@ def items(request):
         items = items.filter(category_id=category_id)
 
     if query:
-        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        items = items.filter(Q(name__icontains=query) | Q(descriprion__icontains=query))
 
     return render(request, 'item/items.html', {
         'items': items,
